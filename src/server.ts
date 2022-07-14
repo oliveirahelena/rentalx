014cc77c512b00f7cee5e7fffd45fa3f9cd9eb7d
Helena Oliveira <helena@informach.com.br>
2022-07-14 07:05:43 -0300

    add typeorm

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
 import { router } from "./routes";
 
+import "./database/data-source";
+
 const app = express();
 
 app.use(express.json());
