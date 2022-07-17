fd18e49e68a4a77bbd816e90b4d1398877ede6b0
Helena Oliveira <helena@informach.com.br>
2022-07-17 06:24:08 -0300

    add users repository to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,12 @@ import { CategoriesRepository } from "../../modules/cars/repositories/typeorm/Ca
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 import { SpecificationsRepository } from "../../modules/cars/repositories/typeorm/SpecificationsRepository";
 
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/typeorm/UsersRepository";
+
+import { IUsersTokensRepository } from "../../modules/accounts/repositories/IUsersTokensRepository";
+import { UsersTokensRepository } from "../../modules/accounts/repositories/typeorm/UsersTokensRepository";
+
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
@@ -14,4 +20,14 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  "UsersTokensRepository",
+  UsersTokensRepository
 );
